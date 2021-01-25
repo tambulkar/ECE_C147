41507fbc38beec48457b9a12571a717ded6d66c6
Tanish Ambulkar <tanish.ambulkar99@gmail.com>
2021-01-24 22:09:51 -0800

    finished svm

--- hw2/nndl/svm.py
+++ hw2/nndl/svm.py
@@ -4,221 +4,246 @@ import pdb
 """
 This code was based off of code from cs231n at Stanford University, and modified for ECE C147/C247 at UCLA.
 """
+
+
 class SVM(object):
+    def __init__(self, dims=[10, 3073]):
+        self.init_weights(dims=dims)
 
-  def __init__(self, dims=[10, 3073]):
-    self.init_weights(dims=dims)
+    def init_weights(self, dims):
+        """
+        Initializes the weight matrix of the SVM.  Note that it has shape (C, D)
+        where C is the number of classes and D is the feature size.
+        """
+        self.W = np.random.normal(size=dims)
 
-  def init_weights(self, dims):
-    """
-	Initializes the weight matrix of the SVM.  Note that it has shape (C, D)
-	where C is the number of classes and D is the feature size.
-	"""
-    self.W = np.random.normal(size=dims)
+    def loss(self, X, y):
+        """
+        Calculates the SVM loss.
 
-  def loss(self, X, y):
-    """
-    Calculates the SVM loss.
-  
-    Inputs have dimension D, there are C classes, and we operate on minibatches
-    of N examples.
-  
-    Inputs:
-    - X: A numpy array of shape (N, D) containing a minibatch of data.
-    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-      that X[i] has label c, where 0 <= c < C.
-  
-    Returns a tuple of:
-    - loss as single float
-    """
-  
-    # compute the loss and the gradient
-    num_classes = self.W.shape[0]
-    num_train = X.shape[0]
-    loss = 0.0
+        Inputs have dimension D, there are C classes, and we operate on minibatches
+        of N examples.
 
-    for i in np.arange(num_train):
-    # ================================================================ #
-    # YOUR CODE HERE:
-	  #   Calculate the normalized SVM loss, and store it as 'loss'.
-    #   (That is, calculate the sum of the losses of all the training 
-    #   set margins, and then normalize the loss by the number of 
-	  #	  training examples.)
-    # ================================================================ #
-      pass
-    
-    # ================================================================ #
-    # END YOUR CODE HERE
-    # ================================================================ #
+        Inputs:
+        - X: A numpy array of shape (N, D) containing a minibatch of data.
+        - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+          that X[i] has label c, where 0 <= c < C.
 
-    return loss
-  
-  def loss_and_grad(self, X, y):
-    """
-	Same as self.loss(X, y), except that it also returns the gradient.
+        Returns a tuple of:
+        - loss as single float
+        """
 
-	Output: grad -- a matrix of the same dimensions as W containing 
-		the gradient of the loss with respect to W.
-	"""
-  
-    # compute the loss and the gradient
-    num_classes = self.W.shape[0]
-    num_train = X.shape[0]
-    loss = 0.0
-    grad = np.zeros_like(self.W)
+        # compute the loss and the gradient
+        num_classes = self.W.shape[0]
+        num_train = X.shape[0]
+        loss = 0.0
 
-    for i in np.arange(num_train):
-    # ================================================================ #
-    # YOUR CODE HERE:
-	#   Calculate the SVM loss and the gradient.  Store the gradient in
-    #   the variable grad.
-    # ================================================================ #
-      pass
+        for i in np.arange(num_train):
+            # ================================================================ #
+            # YOUR CODE HERE:
+            #   Calculate the normalized SVM loss, and store it as 'loss'.
+            #   (That is, calculate the sum of the losses of all the training
+            #   set margins, and then normalize the loss by the number of
+            # 	  training examples.)
+            # ================================================================ #
+            for j in np.arange(num_classes):
+                if j != y[i]:
+                    loss += max(
+                        0, 1 + np.dot(self.W[j], X[i]) - np.dot(self.W[y[i]], X[i])
+                    )
 
-    # ================================================================ #
-    # END YOUR CODE HERE
-    # ================================================================ #
+        loss /= num_train
+        # ================================================================ #
+        # END YOUR CODE HERE
+        # ================================================================ #
 
-    loss /= num_train
-    grad /= num_train
+        return loss
 
-    return loss, grad
+    def loss_and_grad(self, X, y):
+        """
+        Same as self.loss(X, y), except that it also returns the gradient.
 
-  def grad_check_sparse(self, X, y, your_grad, num_checks=10, h=1e-5):
-    """
-    sample a few random elements and only return numerical
-    in these dimensions.
-    """
-  
-    for i in np.arange(num_checks):
-      ix = tuple([np.random.randint(m) for m in self.W.shape])
+        Output: grad -- a matrix of the same dimensions as W containing
+                the gradient of the loss with respect to W.
+        """
 
-      oldval = self.W[ix]
-      self.W[ix] = oldval + h # increment by h
-      fxph = self.loss(X, y)
-      self.W[ix] = oldval - h # decrement by h
-      fxmh = self.loss(X,y) # evaluate f(x - h)
-      self.W[ix] = oldval # reset
-  
-      grad_numerical = (fxph - fxmh) / (2 * h)
-      grad_analytic = your_grad[ix]
-      rel_error = abs(grad_numerical - grad_analytic) / (abs(grad_numerical) + abs(grad_analytic))
-      print('numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error))
+        # compute the loss and the gradient
+        num_classes = self.W.shape[0]
+        num_train = X.shape[0]
+        loss = 0.0
+        grad = np.zeros_like(self.W)
 
-  def fast_loss_and_grad(self, X, y):
-    """
-    A vectorized implementation of loss_and_grad. It shares the same
-	inputs and ouptuts as loss_and_grad.
-    """
-    loss = 0.0
-    grad = np.zeros(self.W.shape) # initialize the gradient as zero
-  
-    # ================================================================ #
-    # YOUR CODE HERE:
-	#   Calculate the SVM loss WITHOUT any for loops.
-    # ================================================================ #
+        for i in np.arange(num_train):
+            # ================================================================ #
+            # YOUR CODE HERE:
+            #   Calculate the SVM loss and the gradient.  Store the gradient in
+            #   the variable grad.
+            # ================================================================ #
+            for j in np.arange(num_classes):
+                z_j = 1 + np.dot(self.W[j], X[i]) - np.dot(self.W[y[i]], X[i])
+                if j != y[i]:
+                    loss += max(0, z_j)
+                    grad[j] += X[i] if z_j > 0 else 0
+                    grad[y[i]] -= X[i] if z_j > 0 else 0
 
-    # ================================================================ #
-    # END YOUR CODE HERE
-    # ================================================================ #
+        # ================================================================ #
+        # END YOUR CODE HERE
+        # ================================================================ #
 
+        loss /= num_train
+        grad /= num_train
 
+        return loss, grad
 
-	# ================================================================ #
-    # YOUR CODE HERE:
-	#   Calculate the SVM grad WITHOUT any for loops.
-    # ================================================================ #
-    
-    # ================================================================ #
-    # END YOUR CODE HERE
-    # ================================================================ #
+    def grad_check_sparse(self, X, y, your_grad, num_checks=10, h=1e-5):
+        """
+        sample a few random elements and only return numerical
+        in these dimensions.
+        """
 
-    return loss, grad
+        for i in np.arange(num_checks):
+            ix = tuple([np.random.randint(m) for m in self.W.shape])
 
-  def train(self, X, y, learning_rate=1e-3, num_iters=100,
-            batch_size=200, verbose=False):
-    """
-    Train this linear classifier using stochastic gradient descent.
+            oldval = self.W[ix]
+            self.W[ix] = oldval + h  # increment by h
+            fxph = self.loss(X, y)
+            self.W[ix] = oldval - h  # decrement by h
+            fxmh = self.loss(X, y)  # evaluate f(x - h)
+            self.W[ix] = oldval  # reset
 
-    Inputs:
-    - X: A numpy array of shape (N, D) containing training data; there are N
-      training samples each of dimension D.
-    - y: A numpy array of shape (N,) containing training labels; y[i] = c
-      means that X[i] has label 0 <= c < C for C classes.
-    - learning_rate: (float) learning rate for optimization.
-    - num_iters: (integer) number of steps to take when optimizing
-    - batch_size: (integer) number of training examples to use at each step.
-    - verbose: (boolean) If true, print progress during optimization.
+            grad_numerical = (fxph - fxmh) / (2 * h)
+            grad_analytic = your_grad[ix]
+            rel_error = abs(grad_numerical - grad_analytic) / (
+                abs(grad_numerical) + abs(grad_analytic)
+            )
+            print(
+                "numerical: %f analytic: %f, relative error: %e"
+                % (grad_numerical, grad_analytic, rel_error)
+            )
 
-    Outputs:
-    A list containing the value of the loss function at each training iteration.
-    """
-    num_train, dim = X.shape
-    num_classes = np.max(y) + 1 # assume y takes values 0...K-1 where K is number of classes
+    def fast_loss_and_grad(self, X, y):
+        """
+        A vectorized implementation of loss_and_grad. It shares the same
+            inputs and ouptuts as loss_and_grad.
+        """
+        loss = 0.0
+        grad = np.zeros(self.W.shape)  # initialize the gradient as zero
 
-    self.init_weights(dims=[np.max(y) + 1, X.shape[1]])	# initializes the weights of self.W
+        # ================================================================ #
+        # YOUR CODE HERE:
+        #   Calculate the SVM loss WITHOUT any for loops.
+        # ================================================================ #
+        N = X.shape[0]
+        W_x = np.dot(self.W, X.T)
+        Z = np.maximum(0, 1 + W_x - W_x[y, np.arange(N)])
+        loss = np.sum(np.sum(Z, axis=0) - 1) / N
+        # ================================================================ #
+        # END YOUR CODE HERE
+        # ================================================================ #
 
-    # Run stochastic gradient descent to optimize W
-    loss_history = []
+        # ================================================================ #
+        # YOUR CODE HERE:
+        #   Calculate the SVM grad WITHOUT any for loops.
+        # ================================================================ #
+        Z = (Z > 0).astype(int)
+        Z[y, np.arange(N)] = -(np.sum(Z, axis=0) - 1)
+        grad = np.dot(Z, X) / N
+        # ================================================================ #
+        # END YOUR CODE HERE
+        # ================================================================ #
 
-    for it in np.arange(num_iters):
-      X_batch = None
-      y_batch = None
+        return loss, grad
 
-      # ================================================================ #
-      # YOUR CODE HERE:
-      #   Sample batch_size elements from the training data for use in 
-      #	  gradient descent.  After sampling,
-      #     - X_batch should have shape: (dim, batch_size)
-	    #     - y_batch should have shape: (batch_size,)
-	    #   The indices should be randomly generated to reduce correlations
-	    #   in the dataset.  Use np.random.choice.  It's okay to sample with
-	    #   replacement.
-      # ================================================================ #
-      
-      # ================================================================ #
-      # END YOUR CODE HERE
-      # ================================================================ #
+    def train(
+        self, X, y, learning_rate=1e-3, num_iters=100, batch_size=200, verbose=False
+    ):
+        """
+        Train this linear classifier using stochastic gradient descent.
 
-      # evaluate loss and gradient
-      loss, grad = self.fast_loss_and_grad(X_batch, y_batch)
-      loss_history.append(loss)
+        Inputs:
+        - X: A numpy array of shape (N, D) containing training data; there are N
+          training samples each of dimension D.
+        - y: A numpy array of shape (N,) containing training labels; y[i] = c
+          means that X[i] has label 0 <= c < C for C classes.
+        - learning_rate: (float) learning rate for optimization.
+        - num_iters: (integer) number of steps to take when optimizing
+        - batch_size: (integer) number of training examples to use at each step.
+        - verbose: (boolean) If true, print progress during optimization.
 
-      # ================================================================ #
-      # YOUR CODE HERE:
-      #   Update the parameters, self.W, with a gradient step 
-      # ================================================================ #
+        Outputs:
+        A list containing the value of the loss function at each training iteration.
+        """
+        num_train, dim = X.shape
+        num_classes = (
+            np.max(y) + 1
+        )  # assume y takes values 0...K-1 where K is number of classes
 
-	    # ================================================================ #
-      # END YOUR CODE HERE
-      # ================================================================ #
+        self.init_weights(
+            dims=[np.max(y) + 1, X.shape[1]]
+        )  # initializes the weights of self.W
 
-      if verbose and it % 100 == 0:
-        print('iteration {} / {}: loss {}'.format(it, num_iters, loss))
+        # Run stochastic gradient descent to optimize W
+        loss_history = []
 
-    return loss_history
+        for it in np.arange(num_iters):
+            X_batch = None
+            y_batch = None
 
-  def predict(self, X):
-    """
-    Inputs:
-    - X: N x D array of training data. Each row is a D-dimensional point.
+            # ================================================================ #
+            # YOUR CODE HERE:
+            #   Sample batch_size elements from the training data for use in
+            # 	  gradient descent.  After sampling,
+            #     - X_batch should have shape: (dim, batch_size)
+            #     - y_batch should have shape: (batch_size,)
+            #   The indices should be randomly generated to reduce correlations
+            #   in the dataset.  Use np.random.choice.  It's okay to sample with
+            #   replacement.
+            # ================================================================ #
+            sample_indices = np.random.choice(np.arange(num_train), batch_size)
+            X_batch = X[sample_indices, :]
+            y_batch = y[sample_indices]
+            # ================================================================ #
+            # END YOUR CODE HERE
+            # ================================================================ #
 
-    Returns:
-    - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-      array of length N, and each element is an integer giving the predicted
-      class.
-    """
-    y_pred = np.zeros(X.shape[1])
+            # evaluate loss and gradient
+            loss, grad = self.fast_loss_and_grad(X_batch, y_batch)
+            loss_history.append(loss)
 
+            # ================================================================ #
+            # YOUR CODE HERE:
+            #   Update the parameters, self.W, with a gradient step
+            # ================================================================ #
+            self.W -= learning_rate * grad
+            # ================================================================ #
+            # END YOUR CODE HERE
+            # ================================================================ #
 
-    # ================================================================ #
-    # YOUR CODE HERE:
-    #   Predict the labels given the training data with the parameter self.W.
-    # ================================================================ #
-    
-    # ================================================================ #
-    # END YOUR CODE HERE
-    # ================================================================ #
+            if verbose and it % 100 == 0:
+                print("iteration {} / {}: loss {}".format(it, num_iters, loss))
 
-    return y_pred
+        return loss_history
 
+    def predict(self, X):
+        """
+        Inputs:
+        - X: N x D array of training data. Each row is a D-dimensional point.
+
+        Returns:
+        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
+          array of length N, and each element is an integer giving the predicted
+          class.
+        """
+        y_pred = np.zeros(X.shape[1])
+
+        # ================================================================ #
+        # YOUR CODE HERE:
+        #   Predict the labels given the training data with the parameter self.W.
+        # ================================================================ #
+        scores = np.dot(self.W, X.T)
+        y_pred = np.argmax(scores, axis=0)
+        # ================================================================ #
+        # END YOUR CODE HERE
+        # ================================================================ #
+
+        return y_pred
